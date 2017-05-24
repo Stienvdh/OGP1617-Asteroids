97f0b979b3d024dd0c79986dfd98b66800726e46
Mansieboy <jarrit.thys@gmail.com>
2017-05-24 19:31:31 +0200

    Add files via upload

--- Asteroid.java
+++ Asteroid.java
@@ -2,20 +2,56 @@ package asteroids.model;
 
 public class Asteroid extends MinorPlanet {
 	
+	/**
+	 * Create a new asteroid with given position, velocity and radius.
+	 * 
+	 * @param	xpos
+	 * 			The position for this asteroid along the x-axis.
+	 * @param	ypos 
+	 * 			The position for this asteroid along the y-axis.
+	 * @param	xvel
+	 * 			The velocity for this asteroid along the x-axis.
+	 * @param	yvel
+	 * 			The velocity for this asteroid along the y-axis.
+	 * @param	radius
+	 * 			The radius for this asteroid.
+	 * @post	The mass density of this new asteroid is the standard density.
+	 * 			|new.getMassDensity() == DENSITY
+	 * @effect	The new asteroid is initialized as an minor planet with the given x position,
+	 * 			y position, x velocity, y velocity and radius.
+	 */
 	public Asteroid(double xpos, double ypos, double xvel, double yvel, double radius) {
 		super(xpos,ypos,xvel,yvel,radius);
 		setMassDensity(DENSITY);
 	}
 	
+	/**
+	 * Set the mass density of this asteroid to the given mass density.
+	 */
 	@Override
 	public void setMassDensity(double massDensity) {
 		this.massDensity=DENSITY;
 	}
 	
+	/**
+	 * Terminate the given ship on collision with this asteroid.
+	 * 
+	 * @effect	Terminate the given ship.
+	 * 			|ship.isTerminated() == True
+	 */
 	public void collideShip(Ship ship) {
 		ship.terminate();
 	}
 	
+	/**
+	 * Terminate this asteroid.
+	 * 
+	 * @post	The new state of this asteroid is terminated, it is removed from it's world and
+	 * 			its world is set to null.
+	 * 			|new.isTerminated() == true
+	 * 			|old.getWorld().contains(this) == false
+	 * 			|new.getWorld() == null
+	 */
 	public void terminate() {
 		this.isTerminated=true;
 		this.getWorld().removeEntity(this);
--- MinorPlanet.java
+++ MinorPlanet.java
@@ -3,7 +3,23 @@ package asteroids.model;
 import asteroids.model.exceptions.IllegalRadiusException;
 
 public abstract class MinorPlanet extends Entity {
-
+	
+	/**
+	 * Create a new minor planet with given position, velocity and radius.
+	 * 
+	 * @param	xpos
+	 * 			The position for this minor planet along the x-axis.
+	 * @param	ypos 
+	 * 			The position for this minor planet along the y-axis.
+	 * @param	xvel
+	 * 			The velocity for this minor planet along the x-axis.
+	 * @param	yvel
+	 * 			The velocity for this minor planet along the y-axis.
+	 * @param	radius
+	 * 			The radius for this minor planet.
+	 * @effect	The new minor planet is initialized as an entity with the given x position,
+	 * 			y position, x velocity, y velocity and radius.
+	 */
 	public MinorPlanet(double xpos, double ypos, double xvel, double yvel, double radius) {
 		super(xpos, ypos, xvel, yvel, radius);
 	}
@@ -24,7 +40,11 @@ public abstract class MinorPlanet extends Entity {
 			throw new IllegalRadiusException(radius);
 		this.radius = radius;
 	}
-
+	
+	/**
+	 * Return the mass of the minor planet.
+	 */
+	
 	@Override
 	public double getMass() {
 		return (4.0/3.0)*Math.PI*Math.pow(getRadius(),3)*getMassDensity();
